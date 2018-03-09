871b5d21dee9935fb5de671b28d8de8e97d7253b
akhiln28 <akhil28111997@gmail.com>
2018-03-09 23:12:48 +0530

    got latlon

---
+++ extract.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+# import jason
+
+DF = pd.read_csv("coutry.txt",sep=";")
+
+df2 = DF[["ISO3166A2","maxlatitude","minlatitude","maxlongitude","minlongitude"]]
+
+# dict = df2.to_dict()
+
+dict2 = {}
+for i in range(0,df2.shape[0]):
+    tempDict = {"maxlatitude":df2.iloc[i]["maxlatitude"],"minlatitude":df2.iloc[i]["minlatitude"],"maxlongitude":df2.iloc[i]["maxlongitude"],"minlongitude":df2.iloc[i]["minlongitude"]  }
+    dict2[str(df2.iloc[i][0])] = tempDict
+
+print(dict2)
+# jason.dump(dict2,open("ConCode.jason","w"))
