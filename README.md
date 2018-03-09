66dd8c167fc1217d2dcb7bc8421d8a9889a345a7
lkoshale <lkoshale@gmail.com>
2018-03-10 04:06:48 +0530

    country lat lon solved in CountryCode.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ontology_assignment2
+
+### CountryCode.js contains function that returns lat lon range for that country
--- extract.py
+++ extract.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-# import jason
+import json
 
 DF = pd.read_csv("coutry.txt",sep=";")
 
@@ -13,5 +13,5 @@ for i in range(0,df2.shape[0]):
     tempDict = {"maxlatitude":df2.iloc[i]["maxlatitude"],"minlatitude":df2.iloc[i]["minlatitude"],"maxlongitude":df2.iloc[i]["maxlongitude"],"minlongitude":df2.iloc[i]["minlongitude"]  }
     dict2[str(df2.iloc[i][0])] = tempDict
 
-print(dict2)
-# jason.dump(dict2,open("ConCode.jason","w"))
+# print(dict2)
+json.dump(dict2,open("out.json","w"))
